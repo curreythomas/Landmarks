e4fab1a545b42ab7efcfdd303ee027e23f8411c5
Currey Thomas <curreyt@ceterus.com>
2019-07-01 15:42:21 -0400

    Initial Commit

---
+++ LandmarkList.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Currey Thomas on 7/1/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList : View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationButton(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+#if DEBUG
+struct LandmarkList_Previews : PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
+#endif
